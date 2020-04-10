e6af9a718f9db37a12470e436aa43636f197908e
Victor Veytsman <vityavv@gmail.com>
2020-04-10 14:49:36 -0400

    Make and package

---
+++ background.js
@@ -0,0 +1,8 @@
+// Massive thanks to https://github.com/deadmund/Custom-New-Tab/blob/master/cnt-main-bg-script.js
+browser.tabs.onCreated.addListener(nt => {
+	console.log(nt.url, nt.openerTabId)
+	if ((nt.url === "about:newtab" || nt.url === "about:blank") && (typeof nt.openerTabId) === "undefined") {
+		browser.tabs.create({openerTabId: nt.id, url: "index.html", active: true})
+		browser.tabs.remove(nt.id)
+	}
+})
